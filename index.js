265c38bb8882a9995d73395d016bd52c9ae2127e
KeotiseuF <curtis.fanchonna@gmail.com>
2024-06-23 05:31:56 +0200

    Setup detect local or prod

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const https = require('https');
 const fs = require('fs');
-require('dotenv').config();
 
 const app1 = require('./app1'); // Piiquante
 const app2 = require('./app2'); // Groupomania
@@ -15,17 +14,27 @@ const MY_PASSWORD_MANGO_DB = process.env.PASSWORD_MANGO_DB;
 const KEY_CERTIF = process.env.KEY_CERTIF;
 const CERTIF = process.env.CERTIF;
 
-const options = {
-  key: fs.readFileSync(KEY_CERTIF),
-  cert: fs.readFileSync(CERTIF),
-};
+const checkIsProd = process.env.PROD === 'true';
+let options = undefined;
+
+if (checkIsProd) {
+  options = {
+    key: fs.readFileSync(KEY_CERTIF),
+    cert: fs.readFileSync(CERTIF),
+  };
+}
 
 mongoose.connect("mongodb+srv://" + MY_ID_MANGO_DB + ":" + MY_PASSWORD_MANGO_DB +"@cluster0.a40ry.mongodb.net/?retryWrites=true&w=majority", 
 { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch((error) => console.error(error));
 
-
-https.createServer(options, app1).listen(port1, () => console.log(`App listening on port ${port1}`));
-https.createServer(options, app2).listen(port2, () => console.log(`App listening on port ${port2}`));
-https.createServer(options, app3).listen(port3, () => console.log(`App listening on port ${port3}`));
+if (checkIsProd) {
+  https.createServer(options, app1).listen(port1, () => console.log(`Prod listening on port ${port1}`));
+  https.createServer(options, app2).listen(port2, () => console.log(`Prod listening on port ${port2}`));
+  https.createServer(options, app3).listen(port3, () => console.log(`Prod listening on port ${port3}`));
+} else {
+  app1.listen(port1, () => console.log(`Local listening on port ${port1}`));
+  app2.listen(port2, () => console.log(`Local listening on port ${port2}`));
+  app3.listen(port3, () => console.log(`Local listening on port ${port3}`));
+}
